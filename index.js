e5a0dc8bef39d4a4128a9d5f8ab8e1d03a2fa74c
Sebastian Marambio <s.marambiosandoval@gmail.com>
2023-03-02 15:37:12 -0300

    aws

--- index.js
+++ index.js
@@ -5,8 +5,6 @@ import './src/models/Clients.js'
 import './src/models/InvoicesDetail.js'
 import './src/models/Invoices.js'
 import './src/models/Products.js'
-import fs from 'fs';
-import https from 'https'
 
 //Pagina que inicia el servidor
 
@@ -16,9 +14,6 @@ async function main() {
         console.log('Connection has been established successfully.');
         //if no exist CREATE tables
         await sequelize.sync({ force: false });
-       /*  await https.createServer({
-            key: fs.readFileSync('cerezasverdes.pem')
-        }, app).listen(8080) */
         app.listen(8080)
         console.log('Servidor en el puerto 8080')
     } catch (error) {
