f0800f15116d391db9a0391e82ef28f6ada0e049
Sebastian Marambio <s.marambiosandoval@gmail.com>
2023-03-02 15:10:27 -0300

    aws

--- src/database/database.js
+++ src/database/database.js
@@ -2,4 +2,4 @@ import { Sequelize } from "sequelize";
 import dotenv from "dotenv"; //traemos las variables de entorno
 dotenv.config({path: '.env'})
 
-export const sequelize = new Sequelize(process.env.SECRET_KEY)
+export const sequelize = new Sequelize('postgres://izcnysnb:v7yWwN4-m0hNwOwfGx0RFZgJ0xX2r7dj@babar.db.elephantsql.com/izcnysnb')
